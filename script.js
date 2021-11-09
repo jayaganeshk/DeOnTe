b9e4610cca52b61ada8c4807ca87f9d09f725064
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2021-11-10 01:37:01 +0530

    app: add nav & header mobile styles

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 import initRouter from "@vipranaraya14/view-router";
 
+const menuBtn = document.querySelector("header .menu-icon");
+const navBar = document.querySelector("nav");
+
 const handleWindowLoadEvent = () => {
   const showView = initRouter("views-container");
 
   showView();
 };
 
+const showNavBar = () => {
+  navBar.classList.add("open");
+};
+
+const hideNavBar = (event) => {
+  if (event.target !== event.currentTarget) return;
+
+  navBar.classList.remove("open");
+};
+
 window.addEventListener("load", handleWindowLoadEvent);
+
+menuBtn.addEventListener("click", showNavBar);
+navBar.addEventListener("click", hideNavBar);
+window.addEventListener("hashchange", hideNavBar);
