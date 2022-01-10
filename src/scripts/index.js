67de0d4b4a41396840a707b6c4c3ae40ade913e0
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2022-01-10 15:24:37 +0530

    app: handle contact form submission

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,7 @@ import initRouter from "@vipranaraya14/view-router";
 
 const menuBtn = document.querySelector("header .menu-btn");
 const navBar = document.querySelector("nav");
+const contactForm = document.getElementById("contact-form");
 
 const handleWindowLoadEvent = () => {
   const showView = initRouter("views-container");
@@ -19,8 +20,30 @@ const hideNavBar = (event) => {
   navBar.classList.remove("open");
 };
 
+const handleContactFormSubmit = (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+
+  const body = new URLSearchParams(formData).toString();
+
+  fetch("https://slhzfd2oof.execute-api.ap-south-1.amazonaws.com/dev/", {
+    method: "POST",
+    body: JSON.stringify(Object.fromEntries(formData)),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Success:", data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+};
+
 window.addEventListener("load", handleWindowLoadEvent);
 
 menuBtn.addEventListener("click", showNavBar);
 navBar.addEventListener("click", hideNavBar);
 window.addEventListener("hashchange", hideNavBar);
+
+contactForm.addEventListener("submit", handleContactFormSubmit);
